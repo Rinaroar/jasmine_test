841c45a5b1b677dcc21457e7a1a2c10327e5ec00
Rinaroar <marina.luxin@gmail.com>
2020-07-04 13:49:58 +0200

    wrote first test

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,18 @@
+describe("Bowling game", function() {
+
+  it("should properly calculate a gutter game", function() {
+    // Player throw twice per turn and has 10 turns (20)
+    // imagine the player didnt touch any bowling pin
+    rollMany(20, 0);
+    expect(game.score()).toEqual(0);
+  });
+
+  it("should properly calculate a strike", function () {
+      // test
+  });
+
+  it("should properly calculate a spare", function () {
+      // test
+  });
+
+});
